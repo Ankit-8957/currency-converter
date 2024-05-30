8952dff3d5d9dd37eba9967bc3f2e8adcdd90064
Ankit-8957 <158561801+Ankit-8957@users.noreply.github.com>
2024-05-30 19:13:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,62 @@ const btn = document.querySelector(".btn");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
+const change = document.querySelector(".fa-solid");
+const fromChange = document.querySelector("#fromSelect");
+const toChange = document.querySelector("#toSelect");
+const from = document.querySelector("#from");
+const to = document.querySelector("#to");
+const fromImg = document.querySelector(".img1");
+const toImg = document.querySelector(".img2");
+
+
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amountValue = amount.value;
+    if (amountValue === "" || amountValue < 1) {
+        amountValue = 1;
+        amount.value = 1;
+    }
+    
+    const URL = `${Base_url}${fromCurr.value.toLowerCase()}.json`;
+    let result = await fetch(URL);
+    let data = await result.json();
+    
+    let fromVal = fromCurr.value.toLowerCase();
+    let toVal = toCurr.value.toLowerCase();
+    let finalData = data[fromVal][toVal];
+   
+    let finalAmount = amount.value * finalData;
+    msg.innerText = `${amount.value} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+             
+    }
+);
+
+function exchange() {
+     let swap = fromImg.src;
+     fromImg.src = toImg.src;
+     toImg.src = swap;
+
+    let temp = fromCurr.value;
+    fromCurr.value = toCurr.value;
+    toCurr.value = temp;
+
+
+    
+    
+}
+
 
 for (let select of dropDown) {
     for (code in countryList) {
@@ -24,52 +80,5 @@ for (let select of dropDown) {
         });
     }
 }
-
-const updateFlag = (element) => {
-    let currCode = element.value;
-    let countryCode = countryList[currCode];
-    // console.log(countryCode);
-    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-    let img = element.parentElement.querySelector("img");
-    img.src = newSrc;
-}
-
-btn.addEventListener("click", async (evt) => {
-    evt.preventDefault();
-    let amount = document.querySelector(".amount input");
-    let amountValue = amount.value;
-    if (amountValue === "" || amountValue < 1) {
-        amountValue = 1;
-        amount.value = 1;
-    }
-
-const URL = `${Base_url}${fromCurr.value.toLowerCase()}.json`;
-let result = await fetch(URL);
-let data = await result.json();
-// let countVal = `.${fromCurr.value.toLowerCase()}.${toCurr.value.toLowerCase()}`;
-let fromVal = fromCurr.value.toLowerCase();
-let toVal = toCurr.value.toLowerCase();
-let finalData = data[fromVal][toVal];
-// console.log(countVal);
-// console.log(data.fromCurr.value.toLowerCase().ada);
- let finalAmount = amount.value * finalData;
- msg.innerText = `${amount.value} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
-
-
-
-
-//   for(let i in val){
-//      if(toCurr.value === i){
-//          console.log(i);
-//      }
-//   }
-// for(let i in countryList){
-//     if(fromCurr.value===i){
-//         console.log(fromCurr.value);
-//     }
-
-}
-);
-
-
+change.addEventListener("click", exchange);
 
